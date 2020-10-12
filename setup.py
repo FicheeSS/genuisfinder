afc6ba789344dd0bfd96352b1523f0e257266193
FicheeSS <theo.ange.bocquet@gmail.com>
2020-10-12 19:13:37 +0200

    init

---
+++ setup.py
@@ -0,0 +1,10 @@
+rom setuptools import find_packages, setup
+setup(
+    name=’genuisfinder’,
+    packages=find_packages(),
+    version='0.1',
+    description='Lyrics finder from Genuis',
+    author='Fichee_SS',
+    license='ACSL',
+    install_requires=['beautifulsoup4']
+)
